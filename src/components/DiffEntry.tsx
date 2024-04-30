9449c827031e3384ec3b0b7493785cc7b0dc72df
joshuamenezes <joshua.menezes@outlook.com>
2024-04-30 18:40:19 -0400

    Refactor to ts

---
+++ src/components/DiffEntry.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { TextField } from '@mui/material';
+
+const DiffEntry = () => {
+    return (
+        <>
+            <TextField id="outlined-basic" label="field1" />
+            <TextField id="outlined-basic" label="field2" />
+        </>
+    );
+};
+
+export default DiffEntry;
