fb4715df91b4cb7e97137b5fd8e0ca8fe8a5a902
joshuamenezes <joshua.menezes@outlook.com>
2024-05-03 17:17:59 -0400

    Add diff view, fix modal size for viewing diff

--- src/components/DiffViewButton.tsx
+++ src/components/DiffViewButton.tsx
@@ -1,22 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Dialog, DialogTitle, DialogContent } from '@mui/material';
 import { useDiffEntryContext } from '../contextProviders/DiffEntryContext';
-import getDiff from '../util/computeDiff';
+import { diffChars } from 'diff';
 
 const DiffViewButton: React.FC = () => {
     const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [diffView, setDiffView] = useState<JSX.Element[]>([]);
 
     const { sharedTextA, sharedTextB } = useDiffEntryContext();
-    const diff = getDiff(sharedTextA, sharedTextB);
+
+    // Recalculate diff whenever sharedTextA or sharedTextB changes
+    // https://www.npmjs.com/package/diff
+    useEffect(() => {
+        const diff = diffChars(sharedTextA, sharedTextB);
+
+        const formattedDiff = diff.map((part, index) => {
+            // Nested ternery ass ... basically assign colors by checking if a character was added, removed, or stayed the same
+            const color = part.added ? 'green' : part.removed ? 'red' : 'grey';
+
+            return (
+                <span key={index} style={{ color }}>
+                    {part.value}
+                </span>
+            );
+        });
+
+        setDiffView(formattedDiff);
+    }, [sharedTextA, sharedTextB]);
+
     return (
         <>
             <Button onClick={() => setModalIsOpen(true)} variant="contained" color="secondary">
                 View Diff
             </Button>
-            <Dialog open={modalIsOpen} onClose={() => setModalIsOpen(false)}>
-                <DialogTitle>How To Use Diff Checker</DialogTitle>
+            <Dialog
+                open={modalIsOpen}
+                onClose={() => setModalIsOpen(false)}
+                maxWidth="xl"
+                fullWidth
+            >
+                <DialogTitle>Diff</DialogTitle>
                 <DialogContent>
-                    <p>{diff}</p>
+                    {diffView.length === 0 ? 'No differences found' : diffView}
                 </DialogContent>
                 <Button
                     onClick={() => setModalIsOpen(!modalIsOpen)}
